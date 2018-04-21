c7a2c6a5f32c34e946f2b26acb966e7879dc539b
Yevgeniy Ivanov <anigeka@gmail.com>
2018-04-21 17:35:07 -0500

    checkpoint complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,11 +47,19 @@ end
   )
 end
 
-user = User.first
- user.update_attributes!(
-   email: 'testtesttesttest1231231@gmail.com',
+admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
+
 
 puts "Seed finished"
 puts "#{User.count} users.created"

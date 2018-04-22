dd424f0b10e6f7c4e0c6c1934d22b68a7b0b7088
Yevgeniy Ivanov <anigeka@gmail.com>
2018-04-21 20:01:11 -0500

    slack help

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,15 @@ require 'random_data'
 
 50.times do
 
-  Post.create!(
+  post = Post.create!(
     user: users.sample,
     topic:  topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
+
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 
 posts = Post.all
@@ -66,6 +69,7 @@ puts "#{User.count} users.created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
 puts "#{Question.count} comments created"
+puts "#{Vote.count} votes created"
 
   Post.find_or_create_by!(
 

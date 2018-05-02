751126abb9f1546ea8422b25faf7e74aa12386f5
Yevgeniy Ivanov <anigeka@gmail.com>
2018-05-01 21:19:25 -0500

    slight revision

--- spec/models/sponsored_post_spec.rb
+++ spec/models/sponsored_post_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe SponsoredPost, type: :model do
   let(:title) { RandomData.random_sentence }
   let(:body) { RandomData.random_paragraph }
   let(:topic) { Topic.create!(name: name, description: description) }
-  let(:post) { topic.posts.create!(title: title, body: body) }
+  let(:post) { topic.sponsored_posts.create!(title: title, body: body, price: rand(100...1000)) }
 
   it { is_expected.to belong_to(:topic) }
 
  describe "attributes" do
-   it "has title and body attributes" do
-   expect(post).to have_attributes(title: title, body: body)
+   it "has title, body and price attributes" do
+   expect(post).to have_attributes(title: title, body: body, price: rand(100...1000))
    end
  end
 end

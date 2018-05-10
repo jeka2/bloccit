177b86936b4ba3a0278186aca2f7527b79f69dff
Yevgeniy Ivanov <anigeka@gmail.com>
2018-05-10 14:20:40 -0500

    slack help

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
+   has_many :posts, dependent: :destroy
    has_many :comments, dependent: :destroy
    has_many :votes, dependent: :destroy
    has_many :favorites, dependent: :destroy
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+   let(:user) { create(:user) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
@@ -21,7 +21,7 @@ RSpec.describe User, type: :model do
 
   describe "attributes" do
     it "should have name and email attributes" do
-      expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+      expect(user).to have_attributes(name: user.name, email: user.email)
     end
 
     it "should format the user's name" do
@@ -102,5 +102,13 @@ RSpec.describe User, type: :model do
      end
    end
 
+   describe ".avatar_url" do
+     let(:known_user) { create(:user, email: "blochead@bloc.io") }
+
+     it "returns the proper Gravatar url for a known email entity" do
+       expected_gravatar = "http://gravatar.com/avatar/bb6d1172212c180cfbdb7039129d7b03.png?s=48"
+       expect(known_user.avatar_url(48)).to eq(expected_gravatar)
+     end
+   end
 
 end
